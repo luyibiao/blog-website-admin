e3171cfac560b513bd0f6a6614619982bd82d3f6
luyibiao <1739717921@qq.com>
2021-01-05 15:00:23 +0800

    优化打包部署

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,27 @@ const scss = `
   @import "~@/assets/scss/variable.scss";
   @import "~@/assets/scss/mixin.scss";
 `
-
+const CompressionPlugin = require('compression-webpack-plugin')
 module.exports = {
   publicPath: '/blog-admin/',
+  productionSourceMap: false, // 不要map文件
   css: {
     loaderOptions: {
       scss: {
         prependData: scss
+      }
+    }
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production' ) {
+      return {
+        plugins: [
+          new CompressionPlugin({
+            test: /\.js$|\.html$|\.css$/,
+            threshold: 10240,
+            deleteOriginalAssets: false
+          })
+        ]
       }
     }
   },
@@ -21,12 +35,12 @@ module.exports = {
     // open: true, //配置自动启动浏览器
     proxy: { // 配置跨域
       '/api': {
-          target: 'http://192.168.100.169:3000',
-          ws: true,
-          changOrigin: true,
-          pathRewrite: {
-              '^/api': ''
-          }
+        target: 'http://192.168.100.169:3000',
+        ws: true,
+        changOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
       }
     }
   },
